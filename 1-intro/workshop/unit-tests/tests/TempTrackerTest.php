6b98fb0359dc2ffb586687813b12979a96aba4ef
prynge <37028634+prynge@users.noreply.github.com>
2022-04-21 15:03:21 +0200

    added some unit tests for temp tracker function  in php

--- 1-intro/workshop/unit-tests/tests/TempTrackerTest.php
+++ 1-intro/workshop/unit-tests/tests/TempTrackerTest.php
@@ -6,6 +6,35 @@ require_once('./src/TempTracker.php');
 
 class TempTrackerTest extends TestCase {
     public function test_something() {
-        $this->assertEqual(42, 42);
+        $this->assertEquals(42, 42);
+    }
+    public function test_insert() {
+        $tempTracker = new TempTracker();
+        $tempTracker->insert(45);
+        $this->assertEquals($tempTracker->get_temps(), [45]);
+    }
+    public function test_get_min() {
+        $tempTracker = new TempTracker();
+        $tempTracker->insert(55);
+        $tempTracker->insert(45);
+        $tempTracker->insert(50);
+        $this->assertEquals($tempTracker->get_min(), 45);
+        
+    }
+    public function test_get_max() {
+        $tempTracker = new TempTracker();
+        $tempTracker->insert(55);
+        $tempTracker->insert(45);
+        $tempTracker->insert(50);
+        $this->assertEquals($tempTracker->get_max(), 55);
+        
+    }
+    public function test_get_mean() {
+        $tempTracker = new TempTracker();
+        $tempTracker->insert(55);
+        $tempTracker->insert(45);
+        $tempTracker->insert(50);
+        $this->assertEquals($tempTracker->get_mean(), 50.0);
+        
     }
 }
