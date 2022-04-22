0363d89a2d4539d8ca7e27bd64bfe3e473824128
Lilian-MMI <lilianouvmmi@gmail.com>
2022-04-22 12:24:16 +0200

    add some test to verify the integrity of the item's sellIn property & update Item Class

--- 2-unit-tests/workshop/js/src/gilded_rose.js
+++ 2-unit-tests/workshop/js/src/gilded_rose.js
@@ -1,27 +1,48 @@
 class Item {
-  constructor(name, sellIn, quality){
+  constructor(name, sellIn, quality) {
     this.name = name;
     this.sellIn = sellIn;
     this.quality = quality;
   }
+
+  get sellIn() {
+    return this._sellIn;
+  }
+
+  set sellIn(value) {
+    if (typeof value !== "number")
+      throw new Error("sellIn must be defined as a number");
+
+    if (value < 0) throw new Error("sellIn must be greater than or equal to 0");
+
+    if (value !== 0 && value % 1 === 0)
+      throw new Error("sellIn must be a positive integer");
+
+    this._sellIn = value;
+  }
 }
 
 class Shop {
-  constructor(items=[]){
+  constructor(items = []) {
     this.items = items;
   }
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+      if (
+        this.items[i].name != "Aged Brie" &&
+        this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
+      ) {
         if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+          if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
             this.items[i].quality = this.items[i].quality - 1;
           }
         }
       } else {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+          if (
+            this.items[i].name == "Backstage passes to a TAFKAL80ETC concert"
+          ) {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].quality + 1;
@@ -35,19 +56,22 @@ class Shop {
           }
         }
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+      if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
       if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+        if (this.items[i].name != "Aged Brie") {
+          if (
+            this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
+          ) {
             if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+              if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
                 this.items[i].quality = this.items[i].quality - 1;
               }
             }
           } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
+            this.items[i].quality =
+              this.items[i].quality - this.items[i].quality;
           }
         } else {
           if (this.items[i].quality < 50) {
@@ -63,5 +87,5 @@ class Shop {
 
 module.exports = {
   Item,
-  Shop
-}
+  Shop,
+};
--- 2-unit-tests/workshop/js/test/gilded_rose.test.js
+++ 2-unit-tests/workshop/js/test/gilded_rose.test.js
@@ -1,9 +1,49 @@
-const {Shop, Item} = require("../src/gilded_rose");
+const { Shop, Item } = require("../src/gilded_rose");
 
-describe("Gilded Rose", function() {
-  it("should foo", function() {
+// name, sellIn, quality
+const itemMock = (...params) => new Item(...params);
+
+describe("Gilded Rose", function () {
+  // Tous les éléments ont une valeur sellIn qui désigne le nombre de jours restant pour vendre l'article.
+  describe("Every item must have a sellIn value", () => {
+    it("should have a valid sellIn value", () => {
+      const items = [itemMock("foo", 0, 0), itemMock("bar", 0, 0)];
+      const shop = new Shop(items);
+      shop.items.forEach((itemShop) => {
+        expect(itemShop.sellIn).toBeDefined();
+        expect(typeof itemShop.sellIn).toBe("number");
+        expect(itemShop.sellIn).toBeGreaterThanOrEqual(0);
+      });
+    });
+
+    it("should throw an error if sellIn is not defined", () => {
+      try {
+        itemMock("foo", undefined, 0);
+      } catch ({ message }) {
+        expect(message).toBe("sellIn must be defined as a number");
+      }
+    });
+
+    it("should throw an error if sellIn is not a positive number", () => {
+      try {
+        itemMock("foo", -1, 0);
+      } catch ({ message }) {
+        expect(message).toBe("sellIn must be greater than or equal to 0");
+      }
+    });
+
+    it("should throw an error if sellIn is not a positive integer", () => {
+      try {
+        itemMock("foo", 1.5, 0);
+      } catch ({ message }) {
+        expect(message).toBe("sellIn must be a positive integer");
+      }
+    });
+  });
+
+  /* it("should foo", function () {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
-  });
+    expect(items[0].name).toBe("foo");
+  }); */
 });
